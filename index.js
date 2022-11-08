e4ccb184bd0d9b78cd59e2ff41076f7dea6aa008
maruf21hossain <marufhossainmunna21@gmail.com>
2022-11-08 15:41:35 +0600

    single service added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -8,15 +9,42 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jfb2tzn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// lifeCare   services
 async function run() {
     try{
+        const serviceCollection = client.db('lifeCare').collection('services');
 
+        app.get('/services', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
+        app.get('/allServices', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices);
+        })
+
+        app.get('/allServices/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
     }
     finally{
 
     }
 }
 run().catch(err => console.error(err))
+
 
 app.get('/', (req,res) =>{
     res.send('Life care server is running');
