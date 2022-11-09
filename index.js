9dbcef8ac082fd2feeac05f506f0d3a129c0fe56
maruf21hossain <marufhossainmunna21@gmail.com>
2022-11-09 12:37:09 +0600

    my reviews get api correction

--- index.js
+++ index.js
@@ -56,6 +56,18 @@ async function run() {
             res.send(review);
         })
 
+        app.get('/myReviews', async(req, res) =>{
+            let query = {};
+            if(req.query.email){
+                query ={
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
