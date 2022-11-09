6f63a402d681b8cf9078d809c67a6a693a8a368c
maruf21hossain <marufhossainmunna21@gmail.com>
2022-11-09 13:01:15 +0600

    my reviews delete api done

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
             res.send(review);
         })
 
+        app.delete('/myReviews/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
